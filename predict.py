260cef1c800b923394ffe13e474f08d48140bda0
darweshfazil <darweshfazilaad@gmail.com>
2021-08-17 14:37:37 +0530

    rename

--- predict.py
+++ predict.py
@@ -3,29 +3,52 @@ import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 
 def value(brand):
-        global BMW = 0
-        global Bajaj = 0
-        global Benelli = 0
-        global Ducati = 0
-        global Harley-Davidson = 0
-        global Hero = 0
-        global Honda = 0
-        global Hyosung = 0
-        global Ideal = 0
-        global Indian = 0
-        global Jawa = 0
-        global KTM = 0
-        global Kawasaki = 0
-        global LML = 0
-        global MV = 0
-        global Mahindra = 0
-        global Rajdoot = 0
-        global Royal Enfield = 0
-        global Suzuki = 0
-        global TVS = 0
-        global Triumph = 0
-        global Yamaha = 0
-        global Yezdi = 0
+        global BMW
+        BMW = 0
+        global Bajaj
+        Bajaj = 0
+        global Benelli
+        Benelli = 0
+        global Ducati
+        Ducati = 0
+        global HarleyDavidson
+        HarleyDavidson = 0
+        global Hero
+        Hero = 0
+        global Honda
+        Honda = 0
+        global Hyosung
+        Hyosung = 0
+        global Ideal 
+        Ideal = 0
+        global Indian 
+        Indian = 0
+        global Jawa
+        Jawa = 0
+        global KTM
+        KTM = 0
+        global Kawasaki
+        Kawasaki = 0
+        global LML 
+        LML = 0
+        global MV
+        MV = 0
+        global Mahindra 
+        Mahindra = 0
+        global Rajdoot 
+        Rajdoot = 0
+        global RoyalEnfield
+        RoyalEnfield = 0
+        global Suzuki
+        Suzuki = 0
+        global TVS
+        TVS = 0
+        global Triumph 
+        Triumph = 0
+        global Yamaha
+        Yamaha = 0
+        global Yezdi 
+        Yezdi = 0
 
         brand = 1
 
@@ -36,7 +59,7 @@ def price_prediction(brand, power, kms, year, owner):
     y = bike["price"]
 
     value(brand)
-    X_test = np.array([kms, owner, year, power, BMW, Bajaj, Benelli, Ducati, Harley-Davidson, Hero, Honda, Hyosung, Ideal, Indian, Jawa, KTM, Kawasaki, LML, MV, Mahindra, Rajdoot, Royal Enfield, Suzuki, TVS, Triumph, Yamaha, Yezdi])
+    X_test = np.array([kms, owner, year, power, BMW, Bajaj, Benelli, Ducati, HarleyDavidson, Hero, Honda, Hyosung, Ideal, Indian, Jawa, KTM, Kawasaki, LML, MV, Mahindra, Rajdoot, RoyalEnfield, Suzuki, TVS, Triumph, Yamaha, Yezdi])
     X_test = X_test.reshape((1,-1))
     
     model = RandomForestRegressor()
